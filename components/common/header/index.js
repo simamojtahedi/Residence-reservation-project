fbf33c0b5f36554df7009a0bc7698e0504b64381
sima <siilenus9@gmail.com>
2022-08-08 21:17:42 +0430

    page header updated

--- components/common/header/index.js
+++ components/common/header/index.js
@@ -1,21 +1,34 @@
 
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
+import Button from '@mui/material/Button';
+import { useAuth, useAuthActions } from '../../../context/AuthContext'
 import styles from './Header.module.scss'
 
 const Header = () => {
+  const user = useAuth()
+  const dispatch = useAuthActions()
+
   return (
     <header className={styles.header}>
       <div className='container'>
         <div className={styles.header_container}>
           <ul>
             <li><Link href='/'><a>صفحــه اصلی</a></Link></li>
-            <li><Link href='/'><a>رزرو فــوری</a></Link></li>
-            <li><Link href='/'><a>اتــاق‌ها</a></Link></li>
+            <li><Link href='/rooms'><a>اتــاق‌ها</a></Link></li>
             <li><Link href='/'><a>دربــاره ما</a></Link></li>
             <li><Link href='/'><a>تمــاس با ما</a></Link></li>
           </ul>
-          <Link href='/'><a  className={styles.account}>حساب کاربــری</a></Link>
+          <div>
+            {user?.user ?
+              <div>
+                <Link href='/'><a className={styles.account}>{user?.user.name}</a></Link>
+                <Button onClick={() => dispatch({type: "SIGNOUT"})}>خروج</Button>
+              </div>
+            :
+              <Link href='/sign-in'><a className={styles.account}>ورود</a></Link>
+            }
+          </div>
         </div>
       </div>
     </header>
